bb17d72f27bca9c5a4f941ddcbfe7b5c79255319
MightyDeb <debapriyamaity2005@gmail.com>
2024-06-09 21:17:34 +0530

    Made up the error page

--- client/src/pages/errorPage.jsx
+++ client/src/pages/errorPage.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-
+import {Link} from "react-router-dom"
 const Error = ()=>{
   return(
-    <div>Error</div>
+    <section className="error-page">
+        <Link to="/" className="error-button">Go Back Home</Link>
+        <h2 className="error-message">Page Not Found</h2>
+    </section>
   )
 }
 
