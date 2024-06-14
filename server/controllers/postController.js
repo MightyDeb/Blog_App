e247a9d8b573fab251e69f41acef9734a8761d99
MightyDeb <debapriyamaity2005@gmail.com>
2024-06-14 10:55:09 +0530

    createPost backend

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -7,7 +7,38 @@ const {v4: uuid}= require('uuid')
 
 //  api/posts
 const createPost = async(req,res,next)=>{
-  res.json("Create Post")
+  try{
+    let {title,category,description}= req.body
+    if(!title || !category || !description || !req.files){
+      return next(new HttpError('Fill in all fields and choose thumbnail', 422))
+    }
+      const {thumbnail}= req.files
+      if(thumbnail.size > 2000000){
+        return next(new HttpError('Thumbnail too big.File should be less than 2mb.'))
+      }
+      let fileName= thumbnail.name;
+      let splittedFilename = fileName.split('.')
+      let newFilename= splittedFileName[0]+uuid()+"."+splittedFilename[splittedFilename.length-1]
+      thumbnail.mv(path.join(__dirname, '..', '/uploads', newFilename), async (err)=>{
+        if(err){
+          return next(new HttpError(err))
+        }else{
+          const newPost= await Post.create({title, category, description, thumbnail: newFilename, creator: req.user.id})
+          if(!newPost){
+            return next(new HttpError("Post couldn't be created.", 422))
+          }
+          //find user and increase post count
+          const currentUser = await User.findById(req.user)
+          const userPostCount = currentUser.posts +1;
+          await User.findByIdAndUpdate(req.user.id, {posts: userPostCount})
+          res.status(201).json(newPost)
+          
+        }
+      })
+  }
+  catch(error){
+    return next(new HttpError(error))
+  }
 }
 //  api/posts
 const getPosts = async(req,res,next)=>{
